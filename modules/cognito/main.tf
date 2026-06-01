cdc6171e4b7aca559f0515aa7efa84dfb00f7bc7
khomma0312 <kijikuroneko@gmail.com>
2026-06-01 18:06:02 +0900

    [fix]Cognitoの設定を手動で変更していた分に合わせて修正

--- modules/cognito/main.tf
+++ modules/cognito/main.tf
@@ -37,12 +37,19 @@ resource "aws_cognito_user_pool_client" "main" {
   allowed_oauth_flows                  = ["code"]
   allowed_oauth_scopes                 = ["openid", "email", "profile"]
   allowed_oauth_flows_user_pool_client = true
+  supported_identity_providers         = ["COGNITO"]
 
   callback_urls = var.callback_urls
   logout_urls   = var.logout_urls
 
   generate_secret     = true
   explicit_auth_flows = ["ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_USER_SRP_AUTH"]
+
+  token_validity_units {
+    access_token  = "minutes"
+    id_token      = "minutes"
+    refresh_token = "days"
+  }
 }
 
 resource "aws_secretsmanager_secret" "cognito_client_secret" {
