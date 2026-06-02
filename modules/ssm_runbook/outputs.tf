d98ef1703adde4044b6088cea1eb669f0d008a9e
khomma0312 <kijikuroneko@gmail.com>
2026-06-02 23:42:20 +0900

    [fix]terraform fmtを実行

--- modules/ssm_runbook/outputs.tf
+++ modules/ssm_runbook/outputs.tf
@@ -1,2 +1,2 @@
 output "document_name" { value = aws_ssm_document.rds_user_creation.name }
-output "document_arn"  { value = aws_ssm_document.rds_user_creation.arn }
+output "document_arn" { value = aws_ssm_document.rds_user_creation.arn }
